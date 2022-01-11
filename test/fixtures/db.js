beb895cff663497e5ffbb99aa6459dafe09af47b
keyy123 <parktwins1@gmail.com>
2022-01-11 03:54:15 -0500

    I did it at last!

--- test/fixtures/db.js
+++ test/fixtures/db.js
@@ -1,3 +1,4 @@
+const Task = require("../../model/task")
 const User = require("../../model/user")
 const jwt = require("jsonwebtoken")
 const mongoose = require("mongoose")
@@ -13,13 +14,55 @@ const userOne = {
     }] 
 }
 
+const userTwoId = new mongoose.Types.ObjectId()
+const userTwo = {
+    _id: userTwoId,
+    name:"test-user2",
+    email:"test-user2@example.com",
+    password: "123456789",
+    tokens:[{
+        token: jwt.sign({_id:userTwoId}, process.env.JWT_SECRET) 
+    }] 
+}
+
+
+const taskOne = {
+    _id:new mongoose.Types.ObjectId(),
+    details: "First Tasks",
+    done: false,
+    owner: userOne._id
+}
+
+const taskTwo = {
+    _id:new mongoose.Types.ObjectId(),
+    details: "Second Tasks",
+    done: true,
+    owner: userTwo._id
+}
+
+const taskThree = {
+    _id:new mongoose.Types.ObjectId(),
+    details: "Third Tasks",
+    done: "false",
+    owner: userOne._id
+}
+
+
 const makeDatabase = async () =>{
  await User.deleteMany({})
+ await Task.deleteMany()
  await User.create(userOne)
+ await User.create(userTwo)
+ await Task.create(taskOne)
+ await Task.create(taskTwo)
+ await Task.create(taskThree)
 }
 
 module.exports = {
 userOneId,
 userOne,
+userTwoId,
+userTwo,
+taskOne,
 makeDatabase
 }
--- test/task.test.js
+++ test/task.test.js
@@ -2,7 +2,7 @@ const Task = require("../model/task")
 const app = require("../app")
 const request = require("supertest")
 const mongoose = require("mongoose")
-const {userOne, userOneId, makeDatabase} = require("./fixtures/db")
+const {userOne, userOneId, userTwo, userTwoId, makeDatabase, taskOne} = require("./fixtures/db")
 
 beforeEach(makeDatabase)
 
@@ -15,5 +15,29 @@ test('Should create a new task', async ()=>{
         done: true
     })
     .expect(200)
+    const task = await Task.findById(res.body._id)
+    expect(task).not.toBeNull()
+    expect(task.done).toEqual("true")
+    // console.log(res.body)
 })
 
+test('Should get all tasks for a user', async()=>{
+    const res = await request(app)
+    .get("/tasks")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .expect(200)
+
+    // console.log(res.body.allTasks.length)
+    expect(res.body.allTasks.length).toBe(2)
+})
+
+test('Should fail to delete another user\'s task', async ()=>{
+    const res = request(app)
+    .delete(`/tasks/${taskOne._id}`)
+    .set("Authorization", `Bearer ${userTwo.tokens[0].token}`)
+    .send()
+    .expect(401)
+
+    const task = await Task.findById(taskOne._id)
+    expect(task).not.toBeNull()
+})
